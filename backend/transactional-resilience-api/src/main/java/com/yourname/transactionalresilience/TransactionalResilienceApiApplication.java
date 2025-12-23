2010394fef9155d7510e280060eeafa84247bb82
viniciuspascucci1993 <vinicius.pascucci1@gmail.com>
2025-12-23 19:39:42 -0300

    chore: bootstrap application with clean architecture and local environment

--- backend/transactional-resilience-api/src/main/java/com/yourname/transactionalresilience/TransactionalResilienceApiApplication.java
+++ backend/transactional-resilience-api/src/main/java/com/yourname/transactionalresilience/TransactionalResilienceApiApplication.java
@@ -1,4 +1,4 @@
-package com.yourname.transactionalresilience.api;
+package com.yourname.transactionalresilience;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/transactional-resilience-api/src/test/java/com/yourname/transactionalresilience/TransactionalResilienceApiApplicationTests.java
+++ backend/transactional-resilience-api/src/test/java/com/yourname/transactionalresilience/TransactionalResilienceApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.yourname.transactionalresilience.api;
+package com.yourname.transactionalresilience;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
