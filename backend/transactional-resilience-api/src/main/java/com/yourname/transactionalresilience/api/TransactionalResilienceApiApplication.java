004abcaa9e60a080909dc5162567ba5c8f12abb1
viniciuspascucci1993 <vinicius.pascucci1@gmail.com>
2025-12-23 19:27:04 -0300

    chore: configure application profiles and database connection

---
+++ backend/transactional-resilience-api/src/main/java/com/yourname/transactionalresilience/api/TransactionalResilienceApiApplication.java
@@ -0,0 +1,13 @@
+package com.yourname.transactionalresilience.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalResilienceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionalResilienceApiApplication.class, args);
+	}
+
+}
---
+++ backend/transactional-resilience-api/src/test/java/com/yourname/transactionalresilience/api/TransactionalResilienceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourname.transactionalresilience.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalResilienceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
